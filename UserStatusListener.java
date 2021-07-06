9ef6ddf0ba73a0ba5853928022c98e2bcf02430b
pinokio-life-like <65345836+pinokio-life-like@users.noreply.github.com>
2021-07-06 13:10:44 +0300

    Update UserStatusListener.java

--- UserStatusListener.java
+++ UserStatusListener.java
@@ -1,4 +1,6 @@
 
+/* this is a user status listener wether offline or online*/
+
 public interface UserStatusListener {
     public void online(String login);
     public void offline(String login);
